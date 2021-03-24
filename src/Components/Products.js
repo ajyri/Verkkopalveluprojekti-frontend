21c1a73afc54edb6a5b465ed3a5779ea8a9cb9c3
Jyri Ahola <n0ahjy00@students.oamk.fi>
2021-03-24 18:24:07 +0200

    added functionality for categories

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -6,8 +6,31 @@ export default function Products({trnro}) {
     // luodaan muuttujat kaikille tuotteille
     
     const [products, setProducts] = useState([]);
-    const categoryName = ["", "Kahvit", "Muut juomat", "Teet", "Leivokset"];
+    const [category, setCategory] = useState([])
 
+    
+    
+    useEffect(() => {
+      let status = 0;
+      fetch(URL + 'categories.php?trnimi=' + trnro)
+      .then(res => {
+        status = parseInt(res.status);
+        return res.json()
+      })
+      .then(
+        (res) => {
+        	if (status === 200) {
+            setCategory(res);
+            
+        	} else {
+          alert(res.error);
+        	}
+        }, (error) => {
+          alert(error);
+        }
+      )
+    }, [])
+    
     useEffect(() => {
       let status = 0;
       fetch(URL + 'categories.php?trnro=' + trnro)
@@ -19,24 +42,26 @@ export default function Products({trnro}) {
         (res) => {
         	if (status === 200) {
             setProducts(res);
-         
+            
         	} else {
           alert(res.error);
         	}
         }, (error) => {
           alert(error);
         }
-      )    
+      )
     }, [])
     return (
         <>
         <div id="listing" className="row border-bottom border-start border-end border-dark pb-2">
-            <h2>{categoryName[trnro]}</h2>
+            <h2>{category.map(category =>(
+              category.trnimi
+            ))}</h2>
             <div className="col-12  p-0 m-0">
             <ul className="list-group-horizontal list-group-flush" id="items">
                 {products.map(item => (
                 // Render item here..
-                <li className="list-group-item img-fluid">
+                <li key={item.tuotenro} className="list-group-item img-fluid">
                 <figure>
                     <img src="/img/kahvi_place.jpg" alt="" />
                     <p>{item.tuotenimi}</p>
