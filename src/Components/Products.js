58a16e73949741f122c017175c70451d75806d9d
Aleksi Nokela <n0noal00@student.oamk.fi>
2021-03-24 10:27:14 +0200

    frontendiin tuotteiden haku backendistä

---
+++ src/Components/Products.js
@@ -0,0 +1,51 @@
+import React, {useState, useEffect} from 'react';
+
+const URL = 'http://localhost/verkkopalvelu/'
+
+export default function Items({trnro}) {
+    // luodaan muuttujat kaikille tuotteille
+    
+    const [products, setProducts] = useState([]);
+    
+    useEffect(() => {
+      let status = 0;
+      fetch(URL + 'index.php')
+      .then(res => {
+        status = parseInt(res.status);
+        return res.json()
+      })
+      .then(
+        (res) => {
+        	if (status === 200) {
+            setProducts(res);
+         
+        	} else {
+          alert(res.error);
+        	}
+        }, (error) => {
+          alert(error);
+        }
+      )    
+    }, [])
+    return (
+        <>
+        <div id="listing" className="row border-bottom border-start border-end border-dark pb-2">
+            <div className="col-12  p-0 m-0">
+            <ul className="list-group-horizontal list-group-flush" id="items">
+                {products.map(item => (
+                // Render item here..
+                <li className="list-group-item img-fluid">
+                <figure>
+                    <img src="/img/kahvi_place.jpg" alt="" />
+                    <figcaption>{item.hinta}</figcaption>
+                    <button className="btn">Lisää koriin</button>
+                </figure>
+                <p>{item.kuvaus}</p>
+                </li>
+                ))}
+            </ul>
+            </div>
+        </div>
+        </>
+    ) 
+}
