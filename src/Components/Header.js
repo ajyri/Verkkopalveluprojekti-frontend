0a0d21af3575a665978e470a84e072c58bb8c16e
ukko hansen-haug <n0hauk00@students.oamk.fi>
2021-03-31 12:06:21 +0300

    vähän siistimpi header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,24 +9,10 @@ export default function Header({cart}) {
     return (
         <>
             <header className="row">
-                <div className="col-md-3 col-sm-6 text-start border-start border-top border-dark pt-2 mb-1">
+                <div className="col-md-3 col-sm-7 text-start pt-2 mb-1">
                     <img id="logo" className="img-fluid" src="/img/axlogo.png" alt="" />
                 </div>
-                <div className="col-md-6 order-md-3 col-sm-6  text-end border-end border-top border-dark pt-3 ">
-                    <a type='button' data-bs-target="#cartItems" data-bs-toggle="dropdown">
-                        <Cart count={cart}/>
-                    </a>
-                    <div className="dropdown-menu" id="cartItems">
-                        <ul>
-                        {cart.map(item =>(
-                            <li>
-                                {item.tuotenimi} {item.qty} 
-                            </li>
-                        ))}
-                        </ul>
-                    </div>   
-                </div>
-                <nav className=" col-md-3 order-md-1 col-sm-3 m-0 navbar navbar-expand-lg border-bottom border-top border-start border-end border-dark">
+                <nav className=" col-md-3 order-md-1 col-sm-6 col-6 m-0 align-self-end navbar navbar-expand-lg">
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -44,6 +30,20 @@ export default function Header({cart}) {
                         </ul>
                     </div>
                 </nav>
+                <div className="col-md-6 order-md-2 col-sm-6 col-6 align-self-md-end text-end pt-2 mb-1">
+                    <a type='button' data-bs-target="#cartItems" data-bs-toggle="dropdown">
+                        <Cart count={cart}/>
+                    </a>
+                    <div className="dropdown-menu" id="cartItems">
+                        <ul>
+                        {cart.map(item =>(
+                            <li>
+                                {item.tuotenimi} {item.qty} 
+                            </li>
+                        ))}
+                        </ul>
+                    </div>   
+                </div>
             </header>
         </>
     )
