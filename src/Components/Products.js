884c06df0d8c850135df6c92fc7f60a1a8332823
Jyri Ahola <n0ahjy00@students.oamk.fi>
2021-04-15 12:43:42 +0300

    Added functionality to order page.

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import Header from './Header.js'
 const URL = 'http://localhost/verkkopalvelu/'
 
 export default function Products ({ trnro, addToCart }) {
